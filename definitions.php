e2b87e3e0326bb1dcc119d650956cf8dba059cb5
Nuwan Madushanka Jaliyagoda <NuwanJ@users.noreply.github.com>
2018-04-04 21:40:07 +0530

    Create definitions.php

---
+++ definitions.php
@@ -0,0 +1,8 @@
+
+
+define("SMS_SENDER_URL", "https://api.dialog.lk/sms/send");
+define("USSD_SENDER_URL", "https://api.dialog.lk/ussd/send");
+
+define("SUBSCRIPTION_URL", "https://api.dialog.lk/subscription/send");
+define("SUB_QUERY_URL", "https://api.dialog.lk/subscription/getStatus");
+define("SUB_BASE_URL", "https://api.dialog.lk/subscription/query-base");
