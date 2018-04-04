96b07a0197cdf1729354aca88f3072445e2c0047
Nuwan Madushanka Jaliyagoda <NuwanJ@users.noreply.github.com>
2018-04-04 21:50:05 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once "definitions.php";
+
+include_once "subscription.php";
+$sub = new Subscription();
+
+$AppId = "APP_000000";
+$Password = "";
+$SubscriberId ="tel:94771234567";
+
+
+
+
+
+?>
